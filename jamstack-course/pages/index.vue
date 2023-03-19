cf13599fbd9e7c60a95f18c5bb265fc069d36ffa
ernesto.maria <ernestomaria93@gmail.com>
2023-03-19 11:44:40 +0100

    static site generation

--- jamstack-course/pages/index.vue
+++ jamstack-course/pages/index.vue
@@ -1,6 +1,9 @@
 <script setup>
 
- const story = await useStoryblok('home', {version:'draft'})
+const story = await useStoryblok('home', { version: 'draft', resolve_relations: 'featuredProjects.projects' },
+    {
+    resolveRelations:'featuredProjects.projects'
+})
 </script>
 <template>
 
